780aef2a7ff3b4e718eeac6c747bc5deb535a08d
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 23:37:36 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -1,7 +1,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include <cstdint>   // uint8_t, uint16_t, uint32_t
+#include <cstdint> // uint8_t, uint16_t, uint32_t
 
 typedef uint8_t  u8;  // Entier non signé sur 8 bits
 typedef uint16_t u16; // Entier non signé sur 16 bits
