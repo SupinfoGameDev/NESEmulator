6737d572b7a7ebce71a7af32220e2cc41055dd30
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 15:37:00 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -10,21 +10,21 @@ typedef unsigned char u8; // Entier non signé sur 8 bits
 template <int size = 8>
 class bit_array
 {
-    public:
-        bit_array()
-        { std::fill(bits.begin(), bits.end(), 0); }
-        
-        bit_array(const bit_array& copy)
-        { bits = copy.bits; }
-        
-        std::array<int, size> to_array() const
-        { return bits; }
-        
-        int& operator[](int idx)
-        { return bits[idx]; }
-        
-    private:
-        std::array<int, size> bits;
+  public:
+    bit_array()
+    { std::fill(bits.begin(), bits.end(), 0); }
+    
+    bit_array(const bit_array& copy)
+    { bits = copy.bits; }
+    
+    std::array<int, size> to_array() const
+    { return bits; }
+    
+    int& operator[](int idx)
+    { return bits[idx]; }
+    
+  private:
+    std::array<int, size> bits;
 };
 
 #endif
