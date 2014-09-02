ff524b762808f824c229e9f5a934a7f1a5d84f78
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 15:59:53 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -1,8 +1,13 @@
+#include <iostream>
+using namespace std;
+
 #ifndef TYPES_H
 #define TYPES_H
 
 #include <algorithm> // std::fill
 #include <array>     // std::array
+#include <initializer_list>
+#include <numeric>  // std::accumulate
 
 #define to_u8(x) ((int)(x & 255))
 
@@ -14,15 +19,24 @@ class bit_array
     public:
         bit_array()
         { std::fill(bits.begin(), bits.end(), 0); }
+        
+        bit_array(std::array<int, size> array)
+        { bits = array; }
       
         bit_array(const bit_array& copy)
         { bits = copy.bits; }
       
         std::array<int, size> to_array() const
         { return bits; }
+        
+        int to_number() const
+        { return std::accumulate(bits.begin(), bits.end(), 0); }
       
         int& operator[](int idx)
         { return bits[idx]; }
+        
+        bit_array& operator=(std::array<int, size> list)
+        { bits = list; return *this; }
       
     private:
         std::array<int, size> bits;
