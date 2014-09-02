0687c7384d8829afcee9e1d0a234c2abd44dd363
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 18:10:45 +0200

    Update types.cc

--- src/types.cc
+++ src/types.cc
@@ -10,3 +10,50 @@ inline int from_binary(int bin)
     }
     return sum;
 }
+
+bit_array::bit_array()
+{
+    std::fill(bits.begin(), bits.end(), 0);
+}
+
+bit_array::bit_array(const bit_array& copy)
+    : bits(copy.bits)
+{
+    set_bits();
+}
+
+bit_array::bit_array(const std::array<int, size>& array)
+    : bits(array)
+{
+    set_bits();
+}
+    
+int bit_array::to_number() const
+{
+    return std::accumulate(bits.begin(), bits.end(), 0);
+}
+
+std::array<int, size> bit_array::to_array() const
+{
+    return bits;
+}
+    
+int& bit_array::operator[](int idx)
+{
+    return bits.at(idx);
+}
+
+bit_array& bit_array::operator=(const std::array<int, size>& list)
+{
+    bits = list;
+    set_bits();
+    return *this;
+}
+
+void bit_array::set_bits()
+{
+    for (auto& bit : bits)
+    {
+        bit = !!bit;
+    }
+}
