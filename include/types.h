ce2edae7c2bbdb7c18ddaa4564a5f17858b13a3b
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 21:10:47 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -11,58 +11,10 @@ typedef uint8_t  u8;  // Entier non signé sur 8 bits
 typedef uint16_t u16; // Entier non signé sur 16 bits
 typedef uint32_t u32; // Entier non signé sur 32 bits
 
-template <int size = 8>
-class bit_array {
-public:
-    bit_array()
-    {
-        std::fill(bits.begin(), bits.end(), 0);
-    }
-    bit_array(const bit_array& copy) : bits(copy.bits)
-    {
-        set_bits();
-    }
-    bit_array(const std::array<int, size>& array) : bits(array)
-    {
-        set_bits();
-    }
-  
-    void reset()
-    {
-        std::fill(bits.begin(), bits.end(), 0);
-    }
-    int to_number() const
-    {
-        return std::accumulate(bits.begin(), bits.end(), 0);
-    }
-    std::array<int, size> to_array() const
-    {
-        return bits;
-        }
-    int& operator[](int idx)
-    {
-        return bits.at(idx);
-    }
-    bit_array& operator=(const std::array<int, size>& list)
-    {
-        bits = list;
-        set_bits();
-        return *this;
-    }
-private:
-    std::array<int, size> bits;
-    void set_bits()
-    {
-        for (auto& bit : bits)
-        {
-            bit = !!bit;
-        }
-    }
-};
+#include "bit_array.h"
+typedef bit_array<8> byte;
 
 #define to_u8(x)  ((int)(x & 0xff))
 #define to_u16(x) ((int)(x & 0xffff))
-
-typedef bit_array<8> byte;
 
 #endif
