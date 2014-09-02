e5957dd0db2bcbb4ba917fcd2116a55421952cd6
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 15:31:36 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -1,8 +1,24 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <array>
+
 #define to_u8(x) ((int)(x & 255))
 
 typedef unsigned char u8; // Entier non signé sur 8 bits
+
+template <int size = 8>
+class BitArray
+{
+    public:
+        BitArray();
+        std::array<int, size> to_array() { return bits; }
+        int& operator[](int idx) { return bits[idx]; }
+        
+    private:
+        std::array<int, size> bits;
+};
+
+typedef BitArray bit_array;
 
 #endif
