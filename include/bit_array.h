3c6498ae35d2b70cb776e40ca1589a5f7bc3e614
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-02 21:10:16 +0200

    Update bit_array.h

--- include/bit_array.h
+++ include/bit_array.h
@@ -9,49 +9,50 @@
 template <int size = 8>
 class bit_array {
 public:
-	bit_array()
-	{
-		std::fill(bits.begin(), bits.end(), 0);
-	}
-	bit_array(const bit_array& copy) : bits(copy.bits)
-	{
-		set_bits();
-	}
-	bit_array(const std::array<int, size>& array) : bits(array)
-	{
-		set_bits();
-	}
-	void reset()
-	{
-		std::fill(bits.begin(), bits.end(), 0);
-	}
-	int to_number() const
-	{
-		return std::accumulate(bits.begin(), bits.end(), 0);
-	}
-	std::array<int, size> to_array() const
-	{
-		return bits;
-	}
-	int& operator[](int idx)
-	{
-		return bits.at(idx);
-	}
-	bit_array& operator=(const std::array<int, size>& list)
-	{
-		bits = list;
-		set_bits();
-		return *this;
-	}
+    bit_array()
+    {
+        std::fill(bits.begin(), bits.end(), 0);
+    }
+    bit_array(const bit_array& copy) : bits(copy.bits)
+    {
+        set_bits();
+    }
+    bit_array(const std::array<int, size>& array) : bits(array)
+    {
+        set_bits();
+    }
+  
+    void reset()
+    {
+        std::fill(bits.begin(), bits.end(), 0);
+    }
+    int to_number() const
+    {
+        return std::accumulate(bits.begin(), bits.end(), 0);
+    }
+    std::array<int, size> to_array() const
+    {
+        return bits;
+        }
+    int& operator[](int idx)
+    {
+        return bits.at(idx);
+    }
+    bit_array& operator=(const std::array<int, size>& list)
+    {
+        bits = list;
+        set_bits();
+        return *this;
+    }
 private:
-	std::array<int, size> bits;
-	void set_bits()
-	{
-		for (auto& bit : bits)
-		{
-			bit = !!bit;
-		}
-	}
+    std::array<int, size> bits;
+    void set_bits()
+    {
+        for (auto& bit : bits)
+        {
+            bit = !!bit;
+        }
+    }
 };
 
 #endif
