11ae4a6d9e4ddc97674d7a859a4ab8e4c40e8cda
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-03 14:33:38 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -2,12 +2,11 @@
 #define TYPES_H
 
 #include <cstdint> // uint8_t, uint16_t, uint32_t
+#include "bit_array.h"
 
 typedef uint8_t  u8;  // Entier non signé sur 8 bits
 typedef uint16_t u16; // Entier non signé sur 16 bits
 typedef uint32_t u32; // Entier non signé sur 32 bits
-
-#include "bit_array.h"
 
 struct Registers
 {
@@ -20,11 +19,23 @@ struct Registers
     static bit_array<16> S;  // Stack Pointer Register
 };
 
-bit_array<8>  Registers::A  = 0;
-bit_array<8>  Registers::X  = 0;
-bit_array<8>  Registers::Y  = 0;
-bit_array<8>  Registers::P  = 0;
-bit_array<16> Registers::PC = 0;
-bit_array<16> Registers::S  = 0;
+bit_array<8>  Registers::A  = 0x00;
+bit_array<8>  Registers::X  = 0x00;
+bit_array<8>  Registers::Y  = 0x00;
+bit_array<8>  Registers::P  = 0x04; // Registers::P[5] = 1
+bit_array<16> Registers::PC = 0x00;
+bit_array<16> Registers::S  = 0x00;
+
+/*
+P Register bits:
+7 - N - Negative Flag
+6 - V - Overflow Flag
+5 - 1 - Always 1 Flag
+4 - B - Break Flag
+3 - D - Decimal Flag
+2 - I - Interrupt Flag
+1 - Z - Zero Flag
+0 - C - Carry Flag
+*/
 
 #endif
