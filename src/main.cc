22670aa2180565fa397e0b503cfa19af40b2bad7
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-03 10:58:01 +0200

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,8 @@ int main()
     int bin = 0b111000110010110011010001; // 11100011 00101100 11010001
     while (bin)
     {
-        int opcode = bin & 0xff;
-        bin >>= 8; // Après lecture de l'opcode, on passe au suivant
+        int opcode = bin & 0xff; // Flag de 255 pour lire 8 bits max à la fois
+        bin >>= 8;               // Après lecture de l'opcode, on passe au suivant
         switch (opcode)
         {
             case 0x00:
