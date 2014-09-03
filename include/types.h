44d270a88aace7aa3a42a2a2ef86cf8dac441306
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-03 10:45:31 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -10,7 +10,4 @@ typedef uint32_t u32; // Entier non signé sur 32 bits
 #include "bit_array.h"
 typedef bit_array<8> byte;
 
-inline u8  to_u8(int x)  { return (x & 0x00ff); }
-inline u16 to_u16(int x) { return (x & 0xffff); }
-
 #endif
