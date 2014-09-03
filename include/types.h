08a0f760fed945580b46054e81a712e0e0bf5b79
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-03 14:09:26 +0200

    Update types.h

--- include/types.h
+++ include/types.h
@@ -10,4 +10,23 @@ typedef uint32_t u32; // Entier non signé sur 32 bits
 #include "bit_array.h"
 typedef bit_array<8> byte;
 
+struct Registers
+{
+    virtual Registers() = 0;
+    static bit_array<8>  A;  // Accumulator Register
+    static bit_array<8>  X;  // X Index Register
+    static bit_array<8>  Y;  // Y Index Register
+    static bit_array<8>  P;  // Processor Status Register
+    static bit_array<16> PC; // Program Counter Register
+    static bit_array<16> S;  // Stack Pointer Register
+    
+};
+
+bit_array<8>  Registers::A  = 0;
+bit_array<8>  Registers::X  = 0;
+bit_array<8>  Registers::Y  = 0;
+bit_array<8>  Registers::P  = 0;
+bit_array<16> Registers::PC = 0;
+bit_array<16> Registers::S  = 0;
+
 #endif
