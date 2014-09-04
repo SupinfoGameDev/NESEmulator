aea80ed602b954bc8b81c2b42e228fc3536d8800
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-04 14:15:52 +0200

    Update instructions.h

--- include/instructions.h
+++ include/instructions.h
@@ -1,33 +1,4 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
-#include "core.h" // Registers
-
-// TODO - Mappings opcode <=> fonction
-
-namespace Cache
-{
-    
-}
-
-template <int size>
-inline void mov(bit_array<size>& reg, int number)
-{
-    if (number < 0)
-    {
-        number = -number;
-    }
-    Registers::P[NEGATIVE_FLAG_ID] = (number < 0)  ? 1 : 0;
-    Registers::P[ZERO_FLAG_ID]     = (number == 0) ? 1 : 0;
-    reg = number;
-}
-
-template <int size1, int size2>
-inline void mov(bit_array<size1>& dest, const bit_array<size2>& reg)
-{
-    int number = reg.to_number();
-    Registers::P[ZERO_FLAG_ID] = (number == 0) ? 1 : 0;
-    dest = number;
-}
-
 #endif
