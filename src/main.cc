a2b88fa6013b10af75f11ca341e4e89f70ef21a3
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-04 22:08:34 +0200

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -5,10 +5,10 @@ int main()
     // Exemple
     int bin = 0b111000110010110011010001; // 11100011 00101100 11010001
     // bin est un exemple de code binaire après lecture de la ROM
-    while (bin) // Tant que le nombre binaire != 0
+    while (bin)
     {
-        int opcode = bin & 0xff; // Flag de 255 pour lire 8 bits max à la fois
-        bin >>= 8;               // Après lecture de l'opcode, on passe au suivant
+        int opcode = bin & 0xff;
+        bin >>= 8;
         switch (opcode)
         {
             case 0x00:
