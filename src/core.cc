a05f135bbafc00838a14ef873784851b27f4cc70
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-04 14:14:48 +0200

    Create core.cc

---
+++ src/core.cc
@@ -0,0 +1,28 @@
+#include "../include/core.h"
+
+namespace
+{
+    inline void SET_NEGATIVE(int& operand)
+    {
+        if (operand < 0)
+        {
+            Registers::P[NEGATIVE_FLAG_ID] = 1;
+            operand = -operand;
+            return;
+        }
+        Registers::P[NEGATIVE_FLAG_ID] = 0;
+    }
+    
+    inline void SET_ZERO(int operand)
+    {
+        Registers::P[ZERO_FLAG_ID] = (number == 0) ? 1 : 0;
+    }
+}
+
+// MOV A, operand
+void LDA(int operand)
+{
+    SET_NEGATIVE(operand);
+    SET_ZERO(operand);
+    Registers::A = operand;
+}
