80f88948ad65c36e0f65b75267106c643220ac1e
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-05 17:05:45 +0200

    Update core.cc

--- src/core.cc
+++ src/core.cc
@@ -1,27 +1,48 @@
 #include "../include/core.h"
 
-namespace
+inline void SET_NEGATIVE(int& operand)
 {
-    inline void SET_NEGATIVE(int& operand)
+    if (operand < 0)
     {
-        if (operand < 0)
-        {
-            Registers::P[NEGATIVE_FLAG_ID] = 1;
-            operand = -operand;
-            return;
-        }
-        Registers::P[NEGATIVE_FLAG_ID] = 0;
+        Registers::P[NEGATIVE_FLAG_ID] = 1;
+        operand = -operand;
+        return;
     }
-    
-    inline void SET_ZERO(int operand)
-    {
-        Registers::P[ZERO_FLAG_ID] = (operand == 0) ? 1 : 0;
-    }
+    Registers::P[NEGATIVE_FLAG_ID] = 0;
+}
+
+inline void SET_ZERO(int operand)
+{
+    Registers::P[ZERO_FLAG_ID] = (operand == 0) ? 1 : 0;
 }
 
 // MOV A, operand
 void LDA(int operand)
 {
+    SET_NEGATIVE(operand);
+    SET_ZERO(operand);
+    Registers::A = operand;
+}
+
+// MOV X, operand
+void LDA(int operand)
+{
+    SET_NEGATIVE(operand);
+    SET_ZERO(operand);
+    Registers::A = operand;
+}
+
+// MOV Y, operand
+void LDA(int operand)
+{
+    SET_NEGATIVE(operand);
+    SET_ZERO(operand);
+    Registers::A = operand;
+}
+
+void AND(int operand)
+{
+    operand &= Registers::A.to_number();
     SET_NEGATIVE(operand);
     SET_ZERO(operand);
     Registers::A = operand;
