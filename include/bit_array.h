08144d3013c3f8349ea0571922c3c46d8160a331
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-06 14:43:09 +0200

    Update bit_array.h

--- include/bit_array.h
+++ include/bit_array.h
@@ -3,84 +3,91 @@
 
 #include <algorithm> // std::fill, std::reverse
 #include <array>     // std::array
+#include <cmath>     // abs
 
 template <int size = 8>
 class bit_array {
 public:
-    bit_array()
-    {
-        std::fill(bits.begin(), bits.end(), 0);
-    }
-    bit_array(const bit_array& copy)
-        : bits(copy.bits)
-    {
-        set_bits();
-    }
-    bit_array(int number)
-    {
-        std::fill(bits.begin(), bits.end(), 0);
-        int i = 0;
-        for (auto& b : bits)
-        {
-            b = !!(number & (1 << i));
-            i++;
-        }
-        std::reverse(bits.begin(), bits.end());
-    }
-    bit_array(const std::array<int, size>& array)
-        : bits(array)
-    {
-        set_bits();
-    }
-  
-    inline void reset()
-    {
-        std::fill(bits.begin(), bits.end(), 0);
-    }
-    inline int to_number() const
-    {
-        int sum = 0;
-        for (int i = bits.size(); i > 0; i--)
-        {
-            sum += bits[i-1] << (bits.size() - i);
-        }
-        return sum;
-    }
-    inline std::array<int, size> to_array() const
-    {
-        return bits;
-    }
-    inline int& operator[](int idx)
-    {
-        return bits.at(idx);
-    }
-    inline bit_array& operator=(int number)
-    {
-       	std::fill(bits.begin(), bits.end(), 0);
-        int i = 0;
-        for (auto& b : bits)
-        {
-            b = !!(number & (1 << i));
-            i++;
-        }
-        std::reverse(bits.begin(), bits.end());
-        return *this;
-    }
-    inline bit_array& operator=(const std::array<int, size>& list)
-    {
-        bits = list;
-        set_bits();
-        return *this;
-    }
+	bit_array()
+	{
+		std::fill(bits.begin(), bits.end(), 0);
+	}
+	bit_array(const bit_array& copy)
+		: bits(copy.bits)
+	{
+		set_bits();
+	}
+	bit_array(int number)
+	{
+		set_number(number);
+	}
+	bit_array(const std::array<int, size>& array)
+		: bits(array)
+	{
+		set_bits();
+	}
+
+	inline void reset()
+	{
+		std::fill(bits.begin(), bits.end(), 0);
+	}
+	inline int to_number() const
+	{
+		std::array<int, size - 1> arr;
+		for (int i {}; i < arr.size(); i++)
+		{
+			arr[i] = bits[i];
+		}
+		int sum = 0;
+		for (int i = arr.size(); i > 0; i--)
+		{
+			sum += arr[i - 1] << (arr.size() - i);
+		}
+		int idx = bits.size() - 1;
+		return (bits[idx] == 0) ? sum : -sum;
+	}
+	inline std::array<int, size> to_array() const
+	{
+		return bits;
+	}
+	inline int& operator[](int idx)
+	{
+		return bits.at(idx);
+	}
+	inline bit_array& operator=(int number)
+	{
+		set_number(number);
+		return *this;
+	}
+	inline bit_array& operator=(const std::array<int, size>& list)
+	{
+		bits = list;
+		set_bits();
+		return *this;
+	}
 private:
-    std::array<int, size> bits;
-    inline void set_bits()
-    {
-        for (auto& bit : bits)
-        {
-            bit = !!bit;
-        }
-    }
+	std::array<int, size> bits;
+	inline void set_bits()
+	{
+		for (auto& bit : bits)
+		{
+			bit = !!bit;
+		}
+	}
+	inline void set_number(int number)
+	{
+		int negative_bit = (number < 0);
+		number = abs(number) & 127; // & ou rabattre valeur à 127 ?
+		std::fill(bits.begin(), bits.end(), 0);
+		int i = 0;
+		for (auto& b : bits)
+		{
+			b = !!(number & (1 << i));
+			i++;
+		}
+		//std::reverse(bits.begin(), bits.end());
+		bits[bits.size() - 1] = negative_bit;
+	}
 };
 
 #endif
