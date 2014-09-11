313a625fcc59809c5fa53dead623eb64af837028
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-11 13:54:11 +0200

    Update core.h

--- include/core.h
+++ include/core.h
@@ -38,6 +38,45 @@ private:
 // 7ème bit = bit de signe => valeur max d'un registre 8 bits = 127 *
 // Et valeur min d'un registre de 8 bits = -127 (127 + bits[7] à 1) *
 // ******************************************************************
+}
+
+/*
+=====================
+|  P Register bits: |
+=====================
+7 - N - Negative Flag  => Vaut 1 si le résultat de toute opération sur A, X ou Y est négatif, 0 sinon
+6 - V - Overflow Flag  => vaut 1 si tout résultat arithmétique est supérieur 127 ou inférieur à -127
+5 - 1 - Always 1 Flag  => Vaut toujours 1
+4 - B - Break Flag     => Vaut 1 si le programme exécute un BRK. A considérer comme toujours égal à 0
+3 - D - Decimal Flag   => La NES n'utilise pas de nombres décimaux. A considérer comme toujours égal à 0
+2 - I - Interrupt Flag => Empêche les interruptions si à 1
+1 - Z - Zero Flag      => Vaut 1 si une opération arithmétique retourne 0
+0 - C - Carry Flag     =>
+*/
+
+const int NEGATIVE_FLAG_ID  = 7;
+const int OVERFLOW_FLAG_ID  = 6;
+const int ALWAYS_1_FLAG_ID  = 5; // A ne normalement pas utiliser
+const int BREAK_FLAG_ID     = 4; // A ne normalement pas utiliser
+const int DECIMAL_FLAG_ID   = 3; // A ne normalement pas utiliser
+const int INTERRUPT_FLAG_ID = 2;
+const int ZERO_FLAG_ID      = 1;
+const int CARRY_FLAG_ID     = 0;
+
+enum AddressingMode
+{
+    Absolute = 1,
+    Accumulator,
+    Immediate,
+    Implied,
+    Indexed,
+    Indirect,
+    PostIndexedIndirect,
+    PreIndexed,
+    Relative,
+    ZeroPage,
+    ZeroPageIndexed
+};
 
 // 6502 assembly opcodes
 /*
@@ -115,18 +154,18 @@ NOP - No Operation
 */
 
 // Storage *******************************************************************
-void LDA(int operand, int mode = 0); // Charge la valeur de operand dans A   *
-void LDX(int operand, int mode = 0); // Charge la valeur de operand dans X   *
-void LDY(int operand, int mode = 0); // Charge la valeur de operand dans X   *
-void STA(int& operand, int mode = 0); // operand = Registers::A              *
-void STX(int& operand, int mode = 0); // operand = Registers::X              *
-void STY(int& operand, int mode = 0); // operand = Registers::Y              *
-void TAX(int mode = 0); // Transfère la valeur de A vers X                   *
-void TAY(int mode = 0); // Transfère la valeur de A vers Y                   *
-void TSX(int mode = 0); // Transfère la valeur de S vers X (S : stack)       *
-void TXA(int mode = 0); // Transfère la valeur de X vers A                   *
-void TXS(int mode = 0); // Transfère la valeur de X vers S                   *
-void TYA(int mode = 0); // Transfère la valeur de Y vers A                   *
+void LDA(int operand, int mode = AddressingMode::Immediate); // A = operand  *
+void LDX(int operand, int mode = AddressingMode::Immediate); // X = operand  *
+void LDY(int operand, int mode = AddressingMode::Immediate); // Y = operand  *
+void STA(int& operand, int mode = AddressingMode::Immediate); // operand = A *
+void STX(int& operand, int mode = AddressingMode::Immediate); // operand = X *
+void STY(int& operand, int mode = AddressingMode::Immediate); // operand = Y *
+void TAX(int mode = AddressingMode::Immediate); // X = A                     *
+void TAY(int mode = AddressingMode::Immediate); // Y = A                     *
+void TSX(int mode = AddressingMode::Immediate); // X = S                     *
+void TXA(int mode = AddressingMode::Immediate); // A = X                     *
+void TXS(int mode = AddressingMode::Immediate); // S = X                     *
+void TYA(int mode = AddressingMode::Immediate); // A = Y                     *
 // ***************************************************************************
 
 // Math **********************************************************************
@@ -134,12 +173,12 @@ void TYA(int mode = 0); // Transfère la valeur de Y vers A                   *
 // ***************************************************************************
 
 // Bitvise *******************************************************************
-void AND(int operand, int mode = 0); // Registers::A = operand & Register::A *
-void ASL(int n, int mode = 0);       // Registers::A = Registers::A << n     *
+void AND(int operand, int mode = AddressingMode::Immediate); // A &= operand *
+void ASL(int n, int mode = AddressingMode::Immediate);       // A <<= n      *
 //                                                                           *
-void EOR(int operand, int mode = 0); // Registers::A = operand ^ Register::A *
-void LSR(int n, int mode = 0);       // Registers::A = Registers::A >> n     *
-void ORA(int operand, int mode = 0); // Registers::A = operand | Register::A *
+void EOR(int operand, int mode = AddressingMode::Immediate); // A ^= operand *
+void LSR(int n, int mode = AddressingMode::Immediate);       // A >>= n      *
+void ORA(int operand, int mode = AddressingMode::Immediate); // A |= operand *
 //                                                                           *
 //                                                                           *
 // ***************************************************************************
@@ -147,44 +186,5 @@ void ORA(int operand, int mode = 0); // Registers::A = operand | Register::A *
 // Note ********************************************************
 // ST[A|X|Y] = "Store [register] in memory"                    *
 // *************************************************************
-}
-
-/*
-=====================
-|  P Register bits: |
-=====================
-7 - N - Negative Flag  => Vaut 1 si le résultat de toute opération sur A, X ou Y est négatif, 0 sinon
-6 - V - Overflow Flag  => vaut 1 si tout résultat arithmétique est supérieur 127 ou inférieur à -127
-5 - 1 - Always 1 Flag  => Vaut toujours 1
-4 - B - Break Flag     => Vaut 1 si le programme exécute un BRK. A considérer comme toujours égal à 0
-3 - D - Decimal Flag   => La NES n'utilise pas de nombres décimaux. A considérer comme toujours égal à 0
-2 - I - Interrupt Flag => Empêche les interruptions si à 1
-1 - Z - Zero Flag      => Vaut 1 si une opération arithmétique retourne 0
-0 - C - Carry Flag     =>
-*/
-
-const int NEGATIVE_FLAG_ID  = 7;
-const int OVERFLOW_FLAG_ID  = 6;
-const int ALWAYS_1_FLAG_ID  = 5; // A ne normalement pas utiliser
-const int BREAK_FLAG_ID     = 4; // A ne normalement pas utiliser
-const int DECIMAL_FLAG_ID   = 3; // A ne normalement pas utiliser
-const int INTERRUPT_FLAG_ID = 2;
-const int ZERO_FLAG_ID      = 1;
-const int CARRY_FLAG_ID     = 0;
-
-enum AddressingMode
-{
-    Absolute = 1,
-    Accumulator,
-    Immediate,
-    Implied,
-    Indexed,
-    Indirect,
-    PostIndexedIndirect,
-    PreIndexed,
-    Relative,
-    ZeroPage,
-    ZeroPageIndexed
-};
 
 #endif
