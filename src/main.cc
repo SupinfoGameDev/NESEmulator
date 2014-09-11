f78895aaf1840609298fecef810caa06baa4cb5f
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-11 14:04:14 +0200

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -18,16 +18,50 @@ int main()
             case 0x00:
                 return 0; // BRK instruction
                 break;
+                
             case 0x01:
                 // ...
                 break;
-            // ...
+                
+            case 0x30:
+                // BPL $           ; Goto $ while X is not -1.
+                break;
+                
+            case 0x4c:
+            case 0x6c:
+                // JMP #$1 #$2     ; Jump #$1#$2 (little-endiant)
+                break;
+                
+            case 0x95:
+                // STA $, X        ; Store A into $ offset with X.
+                break;
+                
+            case 0xa2:
+                // LDX #$
+                break;
+                
+            case 0x0a:
+                ASL();
+                break;
+                
             case 0xa5:
-                // LDA A, $
+                // LDA $
                 break;
+                
             case 0xa9:
-                // LDA A, #$
+                // LDA #$
                 break;
+                
+            case 0xaa:
+                TAX();
+            case 0xad:
+                // LDA A, $ (avec value sur 2 octets)
+                break;
+                
+            case 0xca:
+                // DEX (decrement X)
+                break;
+                
             case 0xff:
                 // ...
                 break;
