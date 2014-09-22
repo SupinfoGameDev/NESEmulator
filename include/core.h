1efbbf3008480218e29e3b0f798410d03d5785e3
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-22 14:42:52 +0200

    Update core.h

--- include/core.h
+++ include/core.h
@@ -10,7 +10,10 @@ typedef uint8_t  u8;  // Entier non signé sur 8 bits
 typedef uint16_t u16; // Entier non signé sur 16 bits
 typedef uint32_t u32; // Entier non signé sur 32 bits
 
+#ifndef DEBUG
 #define DEBUG
+#endif
+
 #ifdef DEBUG
 // Overload de << pour cout sur u8
 #include <iostream> // std::cout, std::endl
