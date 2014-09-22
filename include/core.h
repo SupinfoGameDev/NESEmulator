51e8546b7b1e9ab1bd4aa24a1f8f96d733091ea6
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-22 17:05:38 +0200

    Update core.h

--- include/core.h
+++ include/core.h
@@ -14,23 +14,6 @@ typedef uint32_t u32; // Entier non signé sur 32 bits
 #define DEBUG
 #endif
 
-#ifdef DEBUG
-// Overload de << pour cout sur u8
-#include <iostream> // std::cout, std::endl
-std::ostream& operator<<(std::ostream& stream, const u8& i) {
-    stream << static_cast<int>(i);
-    return stream;
-}
-std::ostream& operator<<(std::ostream& stream, const u16& i) {
-    stream << static_cast<int>(i);
-    return stream;
-}
-std::ostream& operator<<(std::ostream& stream, const u32& i) {
-    stream << static_cast<int>(i);
-    return stream;
-}
-#endif
-
 namespace CPU {
     
 struct Registers
@@ -47,20 +30,6 @@ private:
 };
 }
 
-/*
-=====================
-|  P Register bits: |
-=====================
-7 - N - Negative Flag  => Vaut 1 si le résultat de toute opération sur A, X ou Y est négatif, 0 sinon
-6 - V - Overflow Flag  => vaut 1 si tout résultat arithmétique est supérieur 127 ou inférieur à -127
-5 - 1 - Always 1 Flag  => Vaut toujours 1
-4 - B - Break Flag     => Vaut 1 si le programme exécute un BRK. A considérer comme toujours égal à 0
-3 - D - Decimal Flag   => La NES n'utilise pas de nombres décimaux. A considérer comme toujours égal à 0
-2 - I - Interrupt Flag => Empêche les interruptions si à 1
-1 - Z - Zero Flag      => Vaut 1 si une opération arithmétique retourne 0
-0 - C - Carry Flag     =>
-*/
-
 constexpr int NEGATIVE_FLAG_ID  = 7;
 constexpr int OVERFLOW_FLAG_ID  = 6;
 constexpr int ALWAYS_1_FLAG_ID  = 5; // A ne normalement pas utiliser
@@ -70,4 +39,20 @@ constexpr int INTERRUPT_FLAG_ID = 2;
 constexpr int ZERO_FLAG_ID      = 1;
 constexpr int CARRY_FLAG_ID     = 0;
 
-#endif
+#ifdef DEBUG
+// Overload de << pour cout sur u8
+#include <iostream> // std::cout, std::endl
+std::ostream& operator<<(std::ostream& stream, const u8& i) {
+    stream << static_cast<int>(i);
+    return stream;
+}
+std::ostream& operator<<(std::ostream& stream, const u16& i) {
+    stream << static_cast<int>(i);
+    return stream;
+}
+std::ostream& operator<<(std::ostream& stream, const u32& i) {
+    stream << static_cast<int>(i);
+    return stream;
+}
+#endif // DEBUG
+#endif // CORE_H
