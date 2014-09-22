541bed0514f7c86d3c53ff98ff0d8ef499d3369f
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-22 16:52:57 +0200

    Update core.h

--- include/core.h
+++ include/core.h
@@ -85,38 +85,38 @@ enum AddressingMode
     ZeroPageIndexed
 };
 
-// Storage  *******************************************************************
-void LDA(int operand, int mode = AddressingMode::Immediate); // A = operand   *
-void LDX(int operand, int mode = AddressingMode::Immediate); // X = operand   *
-void LDY(int operand, int mode = AddressingMode::Immediate); // Y = operand   *
-void STA(int operand, int mode = AddressingMode::Immediate); // operand = A   *
-void STX(int operand, int mode = AddressingMode::Immediate); // operand = X   *
-void STY(int operand, int mode = AddressingMode::Immediate); // operand = Y   *
-void TAX(); // Transfère la valeur de A vers X                                *
-void TAY(); // Transfère la valeur de A vers Y                                *
-void TSX(); // Transfère la valeur de S vers X                                *
-void TXA(); // Transfère la valeur de S vers A                                *
-void TXS(); // Transfère la valeur de X vers S                                *
-void TYA(); // Transfère la valeur de Y vers A                                *
-//  ***************************************************************************
+// Storage  ***************************************
+void LDA(int operand, int mode); // A = operand   *
+void LDX(int operand, int mode); // X = operand   *
+void LDY(int operand, int mode); // Y = operand   *
+void STA(int operand, int mode); // operand = A   *
+void STX(int operand, int mode); // operand = X   *
+void STY(int operand, int mode); // operand = Y   *
+void TAX(); // Transfère la valeur de A vers X    *
+void TAY(); // Transfère la valeur de A vers Y    *
+void TSX(); // Transfère la valeur de S vers X    *
+void TXA(); // Transfère la valeur de S vers A    *
+void TXS(); // Transfère la valeur de X vers S    *
+void TYA(); // Transfère la valeur de Y vers A    *
+//  ***********************************************
 
-// Math  **********************************************************************
-//                                                                            *
-//  ***************************************************************************
+// Math  ******************************************
+//                                                *
+//  ***********************************************
 
-// Bitvise  *******************************************************************
-void AND(int operand, int mode = AddressingMode::Immediate); // A &= operand  *
-void ASL(int operand, int mode = AddressingMode::Immediate); // A <<= operand *
-//                                                                            *
-void EOR(int operand, int mode = AddressingMode::Immediate); // A ^= operand  *
-void LSR(int operand, int mode = AddressingMode::Immediate); // A >>= operand *
-void ORA(int operand, int mode = AddressingMode::Immediate); // A |= operand  *
-//                                                                            *
-//                                                                            *
-//  ***************************************************************************
+// Bitvise  ***************************************
+void AND(int operand, int mode); // A &= operand  *
+void ASL(int operand, int mode); // A <<= operand *
+//                                                *
+void EOR(int operand, int mode); // A ^= operand  *
+void LSR(int operand, int mode); // A >>= operand *
+void ORA(int operand, int mode); // A |= operand  *
+//                                                *
+//                                                *
+//  ***********************************************
 
-// Note  ********************************************************
-// ST[A|X|Y] = "Store [register] in memory"                     *
-//  *************************************************************
+// Note  ******************************************
+// ST[A|X|Y] = "Store [register] in memory"       *
+//  ***********************************************
 
 #endif
