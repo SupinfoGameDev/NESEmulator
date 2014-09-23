bb71ed9e16ae930ed034f53b713b10fa6addfcc9
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-23 12:01:30 +0200

    Update graphics.h

--- include/graphics/graphics.h
+++ include/graphics/graphics.h
@@ -20,9 +20,9 @@ const std::array<int, 64> palette = {
 
 struct Color
 {
-    Color() : Color(0, 0, 0)
+    Color() : Color(0, 0, 0) {}
     Color(int color);
-    Color(int _r, int _g, int _b) : r(_r), g(_g), b(_b)
+    Color(int _r, int _g, int _b) : r(_r), g(_g), b(_b) {}
     int r, g, b;
 };
 
