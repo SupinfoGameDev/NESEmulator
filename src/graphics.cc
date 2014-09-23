4538ae681414657ab0b5d88c6b48cc3c3d1f5e6b
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-09-23 15:33:45 +0200

    Update graphics.cc

--- src/graphics.cc
+++ src/graphics.cc
@@ -9,15 +9,16 @@ Color::Color(int color)
 
 Matrix::Matrix(int w, int h)
 {
-	pixels.reserve(w * h);
-	width = w;
-	height = h;
+    pixels.reserve(w * h);
+    width = w;
+    height = h;
 }
+
 int& Matrix::at(int x, int y)
 {
     if (x >= width || y >= height)
     {
         return -1;
     }
-	return pixels[x + y * width];
+    return pixels[x + y * width];
 }
