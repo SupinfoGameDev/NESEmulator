1051f2edaff1bd0f0e793f92a2c83bd0f38635aa
vincent <nothing>
2014-10-10 08:10:06 +0200

    graphics

--- src/graphics.cc
+++ src/graphics.cc
@@ -1,5 +1,5 @@
 #include "../include/graphics/graphics.h"
-
+/*
 void draw_pixel(int x, int y, int color)
 {
     draw_pixel(x, y, Color { color });
@@ -7,14 +7,5 @@ void draw_pixel(int x, int y, int color)
 
 void draw_pixel(int x, int y, Color color)
 {
-    struct Point {
-        int x;
-        int y;
-        Color color;
-    };
-    Point point { x, y, color };
-    if (point.x < 0 || point.y < 0)
-    {
-        return;
-    }
 }
+*/
