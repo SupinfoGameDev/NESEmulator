f88367eaba03723511b3cc73a51ea8e4db8f4546
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-10-13 17:18:38 +0200

    Update bit_array.h

--- include/cpu/bit_array.h
+++ include/cpu/bit_array.h
@@ -8,24 +8,24 @@
 template <int size = 8>
 class bit_array {
 public:
-	bit_array();
-	bit_array(const bit_array& copy);
-	bit_array(int number);
-	bit_array(const std::array<int, size>& array);
+    bit_array();
+    bit_array(const bit_array& copy);
+    bit_array(int number);
+    bit_array(const std::array<int, size>& array);
 
-	void reset();
-	int to_number() const;
-	std::array<int, size> to_array() const;
-	int& operator[](int idx);
-	bit_array& operator=(int number);
-	bit_array& operator=(const bit_array& other);
-	bit_array& operator=(const std::array<int, size>& list);
-	bit_array operator+(const int i);
-	bit_array operator+(const bit_array<size> b);
+    void reset();
+    int to_number() const;
+    std::array<int, size> to_array() const;
+    int& operator[](int idx);
+    bit_array& operator=(int number);
+    bit_array& operator=(const bit_array& other);
+    bit_array& operator=(const std::array<int, size>& list);
+    bit_array operator+(const int i);
+    bit_array operator+(const bit_array<size> b);
 private:
-	std::array<int, size> bits;
-	void set_bits();
-	void set_number(int number);
+    std::array<int, size> bits;
+    void set_bits();
+    void set_number(int number);
 };
 
 #include "bit_array.tpp"
@@ -33,15 +33,15 @@ private:
 template <std::size_t size>
 int operator+(const int i, const bit_array<size> b)
 {
-	return b.to_number() + i;
+    return b.to_number() + i;
 }
 
 // Overload de << pour cout sur u8
 #include <iostream> // std::cout, std::endl
 template <std::size_t size>
 std::ostream& operator<<(std::ostream& stream, const bit_array<size>& b) {
-	stream << b.to_number();
-	return stream;
+    stream << b.to_number();
+    return stream;
 }
 
 #endif
