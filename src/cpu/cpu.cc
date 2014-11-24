dccfb8f91acac132ed28da17b3717ea8c5fd8bb6
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-11-24 19:28:27 +0100

    Passage stack register de 16 à 8 bits

--- src/cpu/cpu.cc
+++ src/cpu/cpu.cc
@@ -7,7 +7,7 @@ std::bitset<8>  registers::A  { 0x00 };  // Accumulator Register
 std::bitset<8>  registers::X  { 0x00 };  // X Index Register
 std::bitset<8>  registers::Y  { 0x00 };  // Y Index Register
 std::bitset<8>  registers::P  { 0x04 };  // Processor Status Register
-std::bitset<8> registers::S   { 0x00 };  // Stack Pointer Register
+std::bitset<8>  registers::S  { 0x00 };  // Stack Pointer Register
 std::bitset<16> registers::PC { 0x00 };  // Program Counter Register
 
 namespace {
