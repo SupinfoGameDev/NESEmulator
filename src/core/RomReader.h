51c3b914babae580d18bd8a81f1845b0f9047f6c
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-11-26 11:24:23 +0100

    Ajout lecture header (en cours)

--- src/core/RomReader.h
+++ src/core/RomReader.h
@@ -4,17 +4,26 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <vector>
+
+struct RomHeader
+{
+    int PrgRomPagesCount;
+    int ChrRomPagesCount;
+};
 
 class RomReader
 {
 public:
     RomReader(const std::string& filename);
     std::map<int, int> map() const;
-
+    RomHeader header() const;
 private:
     void read();
+    bool is_header_ok() const;
     std::map<int, int> mapping_;
     std::ifstream reader_;
+    std::vector<int> header_;
 };
 
 #endif
