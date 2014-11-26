ddc49242967e58801e63b872ce3e33dd2ab8eccf
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-11-26 11:23:35 +0100

    Ajout lecture header

--- src/core/RomReader.cc
+++ src/core/RomReader.cc
@@ -1,14 +1,25 @@
 #include "RomReader.h"
 #include <exception>
 
-RomReader::RomReader(const std::string & filename) : reader_(filename, std::ios::in | std::ios::binary)
+RomReader::RomReader(const std::string & filename)
+    : reader_(filename, std::ios::in | std::ios::binary), header_(0)
 {
     if (!reader_)
     {
         std::string message { "Cannot open " + filename };
-        throw std::ios_base::failure(message.c_str());
+        throw std::exception(message.c_str());
     }
     read();
+}
+
+std::map<int, int> RomReader::map() const
+{
+    return mapping_;
+}
+
+RomHeader RomReader::header() const
+{
+    return RomHeader();
 }
 
 void RomReader::read()
@@ -16,11 +27,31 @@ void RomReader::read()
     int address {};
     while (reader_)
     {
-        mapping_.insert(std::pair<int, int>(address++, reader_.get()));
+        mapping_.insert(std::pair<int, int>(address, reader_.get()));
+        address++;
+    }
+    for (auto& map : mapping_)
+    {
+        header_.push_back(map.second);
+        if (map.first == 15) break;
+    }
+    if (!is_header_ok())
+    {
+        throw std::exception("Bad ROM header");
     }
 }
 
-std::map<int, int> RomReader::map() const
+bool RomReader::is_header_ok() const
 {
-    return mapping_;
+    bool zero { true };
+    for (auto it = header_.begin() + 7; it != header_.end(); it++)
+    {
+        if (*it != 0)
+        {
+            zero = false;
+            break;
+        }
+    }
+    return (zero && header_[0] == 'N' && header_[1] == 'E' && header_[2] == 'S' && header_[3] == 0x1a);
 }
+
